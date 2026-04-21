a1f901bbe11e4f94404224e3990f89417f95c963
Kevin Schumacher <KevinSchumacher@t-online.de>
2026-04-21 15:18:15 +0200

    added classes

---
+++ js/game.js
@@ -0,0 +1,11 @@
+let canvas;
+let ctx;
+let character = new MoveableObjekt();
+
+
+
+
+function init() {
+    canvas = document.getElementById("gameCanvas");
+    ctx = canvas.getContext("2d");
+}
---
+++ js/models/charakter.class.js
@@ -0,0 +1,7 @@
+class Charakter {
+  x;
+  y;
+
+  moveRight() {}
+  jump() {}
+}
