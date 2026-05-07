84159bc8ce001ff18a119ce29f9c6c907efce765
Kevin Schumacher <KevinSchumacher@t-online.de>
2026-05-07 17:24:41 +0200

    style for mobile adjusted, and array for salsa bottle added

--- models/salsa-bottle.class.js
+++ models/salsa-bottle.class.js
@@ -1,9 +1,18 @@
 class SalsaBottle extends Collectables {
-  imagesBottle = [
+  imagesThrowBottle = [
     "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
     "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
     "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
     "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
+  ];
+
+  imagesBottleSplash = [
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png",
   ];
 
   imagesBottleOnGround = ["img/6_salsa_bottle/1_salsa_bottle_on_ground.png"];
